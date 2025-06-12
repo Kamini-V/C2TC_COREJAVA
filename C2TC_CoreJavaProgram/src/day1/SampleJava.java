faf20c404a4a445e31ef766938d421ffbab3c8e4
kamini velu <kamini velu@LAPTOP-URBR26EN.mshome.net>
2025-06-12 10:22:54 +0530

    This project contains my core java practical programs

---
+++ C2TC_CoreJavaProgram/src/day1/SampleJava.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		System.out.print("WELCOME TO JAVA PROGRAM");
+		// TODO Auto-generated method stub
+
+	}
+
+}
