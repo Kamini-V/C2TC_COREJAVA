b4633d20b42f0dc28edcc193079b0819f530272c
kamini velu <kamini velu@LAPTOP-URBR26EN>
2025-06-14 11:29:41 +0530

    Identifier program

---
+++ C2TC_CoreJavaProgram/src/day1/Identifier.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Identifier {
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("Value of the number variable is: " + $number);
+		
+		
+	}
+
+}
