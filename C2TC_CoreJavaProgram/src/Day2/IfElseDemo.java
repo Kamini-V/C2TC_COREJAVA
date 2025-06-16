974296a14c6dfd06b8193b379f3e5925fd56b596
kamini velu <kamini velu@LAPTOP-URBR26EN.mshome.net>
2025-06-16 13:30:39 +0530

    day_2Program

---
+++ C2TC_CoreJavaProgram/src/Day2/IfElseDemo.java
@@ -0,0 +1,14 @@
+package Day2;
+
+public class IfElseDemo {
+	public static void main(String[] args) {
+		int age=11;
+		if(age>=18)
+			System.out.println("You are eligible for voting");
+		else
+			System.out.println("Sorry! You are not eligible");
+		System.out.println("if else demonstration");
+	}
+	
+
+}
